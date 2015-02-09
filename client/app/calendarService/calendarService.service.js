a95736e3a248bf77cc73ce27479d87dcb2210d1d
Steve Rubinstein <slrubinstein@gmail.com>
2015-02-09 09:34:05 -0500

    add start and end dates for calendar searches

--- client/app/calendarService/calendarService.service.js
+++ client/app/calendarService/calendarService.service.js
@@ -33,8 +33,14 @@ function calendarService($http) {
     gapi.client.load('calendar', 'v3').then(makeRequest);
   }
 
-  function makeRequest() {
-    gapi.client.request('/calendar/v3/calendars/primary/events')
+  // 2011-08-30T13:22:53.108Z
+  // date.toISOString()
+  function makeRequest(calendar, start, end) {
+    var calendar = calendar || 'primary',
+        start = start || '2015-02-09T14%3A18%3A43.391Z',
+        end = end || '2015-02-10T14%3A18%3A43.391Z';
+    gapi.client.request('/calendar/v3/calendars/' + calendar + 
+      '/events?timeMin=' + start + '&timeMax=' + end)
     .then(function(result) {
       console.log(result)
     }, function(err) {
